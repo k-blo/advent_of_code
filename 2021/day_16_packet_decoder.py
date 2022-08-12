77273128842d236c7e89e8e66b72baba938b196d
alpin111 <kevinbloch@gmail.com>
2022-08-12 16:02:29 +0200

    tested debugger

--- 2021/day_16_packet_decoder.py
+++ 2021/day_16_packet_decoder.py
@@ -1,29 +1,32 @@
 import os
+
 path = os.path.dirname(os.path.realpath(__file__)) + "/" + "day_16_input.txt"
 data = [line.strip() for line in open(path)]
 
 
+def hxbin(hex_string):  # hex to binary
+    return bin(int(hex_string, 16))[2:].zfill(
+        len(hex_string) * 4)  # zfill makes sure the left trailing 0s are not cut off
 
 
-def hxbin(hex_string): ### hex to binary
-    return bin(int(hex_string, 16))[2:].zfill(len(hex_string)*4) ##zfill makes sure the left trailing 0s are not cut off
+def dez(bin_input):  # binary to decimal
+    return int(bin_input, 2)
 
-def dez(bin): # binary to decimal
-    return int(bin,2)
 
 def Type4(remaining_bits, binary_str=""):
     bits4 = remaining_bits[1:5]
-    if remaining_bits[0] == "1": ### 1 means the package continues
+    if remaining_bits[0] == "1":  # 1 means the package continues
         remaining_bits = remaining_bits[5:]
-        alibistring = Type4(remaining_bits, binary_str=binary_str+bits4)
-        return alibistring ## this is apparently required for the recursive function to return not None
-    else: ## 0 means the package ends
-        return dez(binary_str+bits4), remaining_bits[5:]
-
+        alibistring = Type4(remaining_bits, binary_str=binary_str + bits4)
+        return alibistring  # this is apparently required for the recursive function to return not None
+    else:  # 0 means the package ends
+        return dez(binary_str + bits4), remaining_bits[5:]
 
 
 import math
-def Calc_Values(id, values):
+
+
+def calc_values(id, values):
     if id == 0:
         return sum(values)
     if id == 1:
@@ -41,33 +44,33 @@ def Calc_Values(id, values):
         if values[0] < values[1]:
             return 1
         else:
-            return 0   
+            return 0
     if id == 7:
         if values[0] == values[1]:
             return 1
         else:
-            return 0  
+            return 0
 
 
-def Decode(binary): ## a long binary string
+def Decode(binary):  ## a long binary string
     global versions
 
-    packet_version = dez(binary[:3]) # first 3 bits are packet version
-    packet_type_id = dez(binary[3:6]) # next 3 bits are packet type
+    packet_version = dez(binary[:3])  # first 3 bits are packet version
+    packet_type_id = dez(binary[3:6])  # next 3 bits are packet type
 
-    remaining_bits = binary[6:] ### packet info can be discarded
+    remaining_bits = binary[6:]  ### packet info can be discarded
 
-    versions.append(packet_version) # required for part 1: summing up packet versions contained in binary
+    versions.append(packet_version)  # required for part 1: summing up packet versions contained in binary
 
-    if packet_type_id == 4: #### type 4s contain literal values, encoded in 4 bits
+    if packet_type_id == 4:  #### type 4s contain literal values, encoded in 4 bits
         return Type4(remaining_bits)
 
     else:
-        length_type_ID = remaining_bits[0] # length is encoded in bit-length (0) or number of packages (1)
-        remaining_bits = remaining_bits[1:] # can be cut off
+        length_type_ID = remaining_bits[0]  # length is encoded in bit-length (0) or number of packages (1)
+        remaining_bits = remaining_bits[1:]  # can be cut off
 
-        if length_type_ID == "0": ## here we know the length of the binary string containing the sub-packets
-            length = 15 ### length of the binary saying how many subpackets we get
+        if length_type_ID == "0":  ## here we know the length of the binary string containing the sub-packets
+            length = 15  ### length of the binary saying how many subpackets we get
             sub_packet_size = dez(remaining_bits[:length])
             remaining_bits = remaining_bits[length:]
             sub_packet = remaining_bits[:sub_packet_size]
@@ -89,28 +92,21 @@ def Decode(binary): ## a long binary string
                 value, remaining_bits = Decode(remaining_bits)
                 values += [value]
 
-        return Calc_Values(packet_type_id, values), remaining_bits
-
-
-
-
-
+        return calc_values(packet_type_id, values), remaining_bits
 
 
 def part1_2(hex_string):
-    global versions 
+    global versions
     versions = []
     binary = hxbin(hex_string)
     value, remaining_bits = Decode(binary)
 
-    print ("PART 1:", sum(versions))
-    print ("PART 2:", value)
-
-part1_2(data[0])    
+    print("PART 1:", sum(versions))
+    print("PART 2:", value)
 
 
+part1_2(data[0])
 
 # binary = hxbin("C200B40A82")
 # print(binary)
 # print(Decode(binary))
-
--- WISDOM.py
+++ WISDOM.py
@@ -3,63 +3,57 @@
 
 #### sum of iterable
 print(
-    sum([1,1,1])
+    sum([1, 1, 1])
 )
 
 print(
-    sum([1,1,1],10) ### start adding from 10
+    sum([1, 1, 1], 10)  ### start adding from 10
 )
-
 
 ### prod of iterable
 import math
 
-print (
-    math.prod([1,5,9,1,1,1])
-    ) #### prod returns product multiple of a list
+print(
+    math.prod([1, 5, 9, 1, 1, 1])
+)  #### prod returns product multiple of a list
 
 ### all
-print (
-    all([True, 1, 1, 0, False]) ### returns true if all items in list are True
+print(
+    all([True, 1, 1, 0, False])  ### returns true if all items in list are True
 )
-
 
 ### any
-print (
-    any([True, 1, 1, 0, False]) ### returns true if one items in list is True
+print(
+    any([True, 1, 1, 0, False])  ### returns true if one items in list is True
 )
 
-
-
 ### enumerate
-print (
+print(
     list(
-    enumerate("milk", 10) ### adds counter to an iterable
+        enumerate("milk", 10)  ### adds counter to an iterable
     )
 )
 
-
-
 #### string splitting
-print (   "miau".split("i")   )
+print("miau".split("i"))
 
 #### get every xth character
-print (   "abcdefgh"[::2]  ) 
+print("abcdefgh"[::2])
 
 ### multipli string
-print ("a" * 4)
+print("a" * 4)
 
 ### until the last 2 characters
-print ("abcdefgh"[:-2])
+print("abcdefgh"[:-2])
 
 ### the last 2 characters
-print ("abcdefgh"[-2:])
+print("abcdefgh"[-2:])
 
 ### the first 2 characters
-print ("abcdefgh"[:2])
+print("abcdefgh"[:2])
 
 ### after the first 2 characters
-print ("abcdefgh"[2:])
+print("abcdefgh"[2:])
 
 # insert
 string = "this is a string"
@@ -76,18 +70,12 @@ print(string[i:])
 string = string[:i] + "INSERT" + string[i:]
 print(string)
 
-
-
 ### this also works for lists
-print (list(range(0,10)[2:]))
-
-
+print(list(range(0, 10)[2:]))
 
 ### modulo #
-print (3 % 3) ### returns zero because divisble and remainder is 0
-print (3 % 2) ### returns zero because not divisble and remainder is 1
-
-
+print(3 % 3)  ### returns zero because divisble and remainder is 0
+print(3 % 2)  ### returns zero because not divisble and remainder is 1
 
 ### dictionaries
 
@@ -95,285 +83,266 @@ dict0 = {
     "height": "178cm"
 }
 
-print(dict0.get("height") )
-print(dict0.get("height", "").startswith("1") ) ### return True if value starts with 1
-print(dict0.get("height", "").endswith("cm") ) ### return True if value ends with cm
-
+print(dict0.get("height"))
+print(dict0.get("height", "").startswith("1"))  ### return True if value starts with 1
+print(dict0.get("height", "").endswith("cm"))  ### return True if value ends with cm
 
 ### find largest key
 dict1 = {"a key": 19, "another": 299}
 largest_key = max(dict1, key=dict1.get)
 
-
-
 #### SETS! use sets to remove dublicates
-print ( set([1,2,3,1,2,5])  )
+print(set([1, 2, 3, 1, 2, 5]))
 
 ### string.join
-print ("string".join(["1","b", "endless list of strings"]))
-print ("".join(["1","b", "  endless list of strings"])) #### start string can be empty
+print("string".join(["1", "b", "endless list of strings"]))
+print("".join(["1", "b", "  endless list of strings"]))  #### start string can be empty
 
 
 def Alternative_For_Loop_Writing():
     return list(number + 1
-        for number in [1,3,6,7,8,12])
+                for number in [1, 3, 6, 7, 8, 12])
 
 
-print (Alternative_For_Loop_Writing())
-
+print(Alternative_For_Loop_Writing())
 
 #### the sum of an empty list is 0
-print (sum([]))
+print(sum([]))
 
 ### no iteritems necessary if you don't need the value
-for i in {"it":1}:
-    print (i)
-
-
+for i in {"it": 1}:
+    print(i)
 
 ###### map: apply function for each element
 
-print (list(map(float,[0,1,2])))
+print(list(map(float, [0, 1, 2])))
+
 
 def myfunction(number):
     return number * 3
-print (list(map(myfunction,[0,1,2])))
 
+
+print(list(map(myfunction, [0, 1, 2])))
 
 #### you can include conditionals in list comprehensions
 print([i == 1 for i in range(9)])
 
 #### sum will also add truthy values as 1
-print(  sum([True, 1, False, 99])  )
-
-
+print(sum([True, 1, False, 99]))
 
 from collections import namedtuple
-Classlike = namedtuple("somestring","x y z")
-instance = Classlike(99,9,9999)
-print (instance.z)
-print (instance[0])
 
+Classlike = namedtuple("somestring", "x y z")
+instance = Classlike(99, 9, 9999)
+print(instance.z)
+print(instance[0])
 
 from itertools import permutations, combinations, combinations_with_replacement
+
 ### pays attention to order, generates more results
-print ("PERMUTATIONS: ", list(permutations([1,2,3,4,5],2)) )
+print("PERMUTATIONS: ", list(permutations([1, 2, 3, 4, 5], 2)))
 ### doesn't add (5,1) if (1,5) is already in there
-print ("COMBINATIONS: ",  list(combinations([1,2,3,4,5],2)) )
-print ("combinations_with_replacement: ",  list(combinations_with_replacement([1,2,3,4,5],2)) )
+print("COMBINATIONS: ", list(combinations([1, 2, 3, 4, 5], 2)))
+print("combinations_with_replacement: ", list(combinations_with_replacement([1, 2, 3, 4, 5], 2)))
 
 ### for coordinates
-print ("PERMUTATIONS: ", list(permutations([0,1,-1],2)) )
-print ("COMBINATIONS: ",  list(combinations([0,1,-1],2)) )
-print ("combinations_with_replacement: ",  list(combinations_with_replacement([0,1,-1],2)) )
-
-
-
-
+print("PERMUTATIONS: ", list(permutations([0, 1, -1], 2)))
+print("COMBINATIONS: ", list(combinations([0, 1, -1], 2)))
+print("combinations_with_replacement: ", list(combinations_with_replacement([0, 1, -1], 2)))
 
 ##### this is again different from permutations and combinations
 from itertools import product
-cartesian_product = list(product([0,1], [1,0])) 
-print (cartesian_product)
+
+cartesian_product = list(product([0, 1], [1, 0]))
+print(cartesian_product)
 
 ### can be used to get adjacent coordinates
 from itertools import product
-surrounding_coordinates = list(product([0,1,-1],[1,-1,0])) 
-surrounding_coordinates.remove((0,0))
-print (surrounding_coordinates)
+
+surrounding_coordinates = list(product([0, 1, -1], [1, -1, 0]))
+surrounding_coordinates.remove((0, 0))
+print(surrounding_coordinates)
+
 
 ### returns surrounding coordinates, cuts off those going outside the grid (below 0 or above max)
-def Adjacent(x,y, xmax, ymax):
-    return [(xd+x,yd+y) for xd, yd in surrounding_coordinates
-            if not -1 in [xd+x,yd+y] 
-            and xd+x <= xmax 
-            and yd+y <= ymax]
+def Adjacent(x, y, xmax, ymax):
+    return [(xd + x, yd + y) for xd, yd in surrounding_coordinates
+            if not -1 in [xd + x, yd + y]
+            and xd + x <= xmax
+            and yd + y <= ymax]
 
-print ("ADJACENT: ", Adjacent(2, 2, 4, 2)) ### will cut off coordinates below y=2
-print ("ADJACENT: ", Adjacent(2, 2, 4, 3)) ### will cut off nothing and return all 8 surrounding coordinates
 
-### delete list items by index
+print("ADJACENT: ", Adjacent(2, 2, 4, 2))  ### will cut off coordinates below y=2
+print("ADJACENT: ", Adjacent(2, 2, 4, 3))  ### will cut off nothing and return all 8 surrounding coordinates
+
+# delete list items by index
 a_list = [1, "evil_value", 9]
-del a_list[1] # => [1,9]
+del a_list[1]  # => [1,9]
 
 
-### recursive_function
-def recursive_function(input):
-    input += 1
-    if input < 3:
-        input = recursive_function(input)
-        return input
-        
-    return "RECURSION FINISHED: " + str(input)
+# recursive_function
+def recursive_function(input_rec):
+    input_rec += 1
+    if input_rec < 3:
+        input_rec = recursive_function(input_rec)
+        return input_rec
 
-print (recursive_function(1))
+    return "RECURSION FINISHED: " + str(input_rec)
 
 
+print(recursive_function(1))
 
 # reverse the order of list elements
 prime_numbers = [2, 3, 5, 7]
 prime_numbers.reverse()
 
-
 # reverse string or list
 txt = "Hello World"[::-1]
-reverse = [1,2,3][::-1]
+reverse = [1, 2, 3][::-1]
 
-
-### second to second last item
-print([0,1,2,4][1:-1])
+# second to second last item
+print([0, 1, 2, 4][1:-1])
 print("string"[1:-1])
 
-
-#### range
+# range
 # count from 0 to 9, 10 items
 for i in range(10):
     print(i)
 
 # countdown from 9 to 0, 10 items
-for i in range(9,-1,-1):
+for i in range(9, -1, -1):
     print(i)
 
+if 9 in set([1, 2, 3, 9, 9]):  # function call / better use literal: {1, 2, 3, ...}
+    print("SETS ARE EXTREMELY FASTER THAN LISTS WHEN CHECKING IF ITEM IS IN LIST/SET")
+    print("but not when iterating")
 
-if 9 in set([1,2,3,9,9]):
-    print ("SETS ARE EXTREMELY FASTER THAN LISTS WHEN CHECKING IF ITEM IS IN LIST/SET")
-    print ("but not when iterating")
+# dictionary from tuples
+dictionary = dict([("key1", "value1"), ("key2", "value2"), ])
+print(dictionary)
 
+# dictionary comprehension
+dictionary = {x: x + 1 for x in [1, 2]}
 
-
-#### dictionary from tuples
-dictionary = dict([("key1","value1"), ("key2","value2"), ])
-print (dictionary)
-
-#### dictionary comprehension
-dictionary = {x:x+1 for x in [1,2]}
-### switch keys / values
-dictionary = {y:x for x,y in dictionary.items()} ## dictionary comprehension to switch keys/values
-
+# switch keys / values
+dictionary = {y: x for x, y in dictionary.items()}  ## dictionary comprehension to switch keys/values
 
 messy_list = [9, 6, 8, 33]
-### sort ascending order
+# sort ascending order
 messy_list.sort()
-### sort descending order
+# sort descending order
 messy_list.sort(reverse=True)
 
-
-
-## find substrings
+# find substrings
 string = "a long string 1 2 3 4 5 6 7 8 9"
 if ("1" or "2") in string:
     substrings_found = True
-#### if you need to find many substrings
+# if you need to find many substrings
 substrings_we_search = ["1", "2", "7", "9"]
-if any(substring in string for substring in substrings_we_search): #iterator that searches for substrings
+if any(substring in string for substring in substrings_we_search):  # iterator that searches for substrings
     substrings_found = True
 
-
-
 # https://www.pythonsheets.com/notes/python-generator.html
-####### generators empty themselves
+# generators empty themselves
 gen = (i for i in "this_is_a_generator")
 print(list(gen))
 print(list(gen))
 
-
 g = (x for x in range(6))
-a,*c = g
+a, *c = g
 print(c)
 
-
 print(*(x for x in range(3)))
+
 
 def Generator():
     yield from range(5)
 
-print (list(Generator()))
+
+print(list(Generator()))
 print(*Generator())
 for i in Generator():
-    print (i)
+    print(i)
 
-
-
-
-### flatten lists
-nested_list = [[1,2],[3,4]]
+# flatten lists
+nested_list = [[1, 2], [3, 4]]
 flatten_list = [val for sublist in nested_list for val in sublist]
-print (flatten_list)
+print(flatten_list)
 
-### find out how deeply nested a list is
-depth = lambda L: isinstance(L, list) and max(map(depth, L))+1
+# find out how deeply nested a list is
+depth = lambda L: isinstance(L, list) and max(map(depth, L)) + 1
 
-### count element in list (also works for strings)
+# count element in list (also works for strings)
 how_many_a = ["a", "list", "with", "a"].count("a")
 print("string is this".count("string"))
 
+# find largest tuple
+tuple_list = [(1, 6), (99, 1)]
+largest_tuple_by_first_value = max(tuple_list, key=lambda i: i[0])
+largest_tuple_by_second_value = max(tuple_list, key=lambda i: i[1])
+print("largest tuple", largest_tuple_by_second_value)
 
-## find largest tuple
-tuple_list = [(1,6), (99,1) ]
-largest_tuple_by_first_value = max(tuple_list, key=lambda i:i[0])
-largest_tuple_by_second_value = max(tuple_list, key=lambda i:i[1])
-print ("largest tuple", largest_tuple_by_second_value)
 
-### add tuples together
+# add tuples together
 def Add_Tuples(tuple1, tuple2):
     return tuple(sum(x) for x in zip(tuple1, tuple2))
 
 
-### [::2] every other value, starting with the first
-print([1,2,3,4][::2])
+# [::2] every other value, starting with the first
+print([1, 2, 3, 4][::2])
+
+# read file in one line: [line.strip() for line in open(path)]
 
 
-## read file in one line: [line.strip() for line in open(path)]
-
-
-
-# REDUCE
+# reduce
 from functools import reduce
 
-def my_return_function(a,b):
-    print(a + b )
-    return a+b
-reduce(my_return_function, [1,2,3,4,5,6,7,8])
+
+def my_return_function(a, b):
+    print(a + b)
+    return a + b
 
 
+reduce(my_return_function, [1, 2, 3, 4, 5, 6, 7, 8])
 
-######## factorial
-print ("FACTORIAL: ")
-factorial = lambda num : 1 if num <= 1 else num*factorial(num-1)
-print (factorial(5))
-
+# factorial
+print("FACTORIAL: ")
+factorial = lambda num: 1 if num <= 1 else num * factorial(num - 1)
+print(factorial(5))
 
 ### forloop one liner
 N = 1
 for i in range(3): N += i
 print(N)
 
-
 ### forloop one liner with conditions
 N = 1
-for i in range(3): N += i if i == 1 else 100
+for i in range(3): N += i if i == 1 else 100  # N+1 if i == 1 otherwise i+100
 print(N)
 
-
 ## lambda arguments : expression
-x = lambda a : a + 1 if a == 5 else 100
-print(x(5)) 
-print(x(1)) 
+x = lambda a: a + 1 if a == 5 else 100
+print(x(5))
+print(x(1))
 
-## count iterables (works for lists, dicts, strings)
+# count iterables (works for lists, dicts, strings)
 from collections import Counter
+
 print(Counter("an iterable").most_common())
 print(Counter("an iterable").most_common()[0])
 
 # binaries to decimal
 print(int("010101", 2))
 
-def hxbin(hex_string): ### hex to binary
-    return bin(int(hex_string, 16))[2:].zfill(len(hex_string)*4) ##zfill makes sure the left trailing 0s are not cut off
+
+def hxbin(hex_string):  # hex to binary
+    return bin(int(hex_string, 16))[2:].zfill(
+        len(hex_string) * 4)  # zfill makes sure the left trailing 0s are not cut off
 
 
 # numpy
 import numpy as np
+
 a = np.arange(10).reshape(2, 5)
 print(a)
 print(a.shape)
@@ -382,16 +351,16 @@ print(a.size)
 
 ## multidimensional splicing
 # from both elements, return index 2:
-print(a[0:2, 2]) 
+print(a[0:2, 2])
 
 ## return the matrix from index 2 to 4
-print(a[0:2, 2:4]) 
-
+print(a[0:2, 2:4])
 
 
 ##### assert
 def function_to_debug(input):
     return input + 1
+
 
 test_input = 2
 test_result = 3
